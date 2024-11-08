995946579406ad0a5f09e4e126f1b68bca285d40
Developer <johndoe@johndoe.johndoe>
2024-11-08 18:27:00 -0500

    Init. OrderedPairAnalysis

---
+++ src/OrderedPairAnalysis.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * This class checks if the set of ordered pairs is Transitive, Reflexive, Symmetric.
+ * ... Also checks if the set of ordered pairs is an equivalence relation, if so,
+ * ... find all equivalence classes.
+ */
+public class OrderedPairAnalysis {
+    /**
+     * Spawn point
+     * @param args Useless
+     */
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        try {
+            
+        }catch(Exception e) {
+            e.printStackTrace(); // Just throw the exception
+        }finally {
+            S.close(); // Close the scanner no matter what
+        }
+    }
+}
