5f9de442a22c082216dbf1a21e8087e622f039bc
Developer <johndoe@johndoe.johndoe>
2024-11-08 18:45:39 -0500

    General constructor

--- src/lib/Pair.java
+++ src/lib/Pair.java
@@ -7,6 +7,16 @@ public class Pair<T> {
     public T second;
 
     /**
+     * General constructor to initalize the pair
+     * @param f First element
+     * @param s Second Element
+     */
+    public Pair(T f, T s) {
+        first = f;
+        second = s;
+    }
+
+    /**
      * Checks equality between two pairs
      * @param in The pair to be compared to
      * @return Whether the first and second elements of the first pair equal the first and second elements of the second pair, respectively.
