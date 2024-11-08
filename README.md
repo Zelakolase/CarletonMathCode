2c7d15fcf560d88f995b183a0fdbc6ffe2bb2b8c
Developer <johndoe@johndoe.johndoe>
2024-11-08 18:09:30 -0500

    Start

---
+++ README.md
@@ -0,0 +1,8 @@
+# Carleton Math Code repository
+This repository stores all code files that I use during my studies at Carleton for excercise purposes. Running the files is trivial.
+
+## Why did I make this repo?
+Two birds with one stone. I get to be challenged almost daily with new ideas, and I deepen my understanding in the courses that I take.
+
+# Class list
+TBD
---
+++ src/App.java
@@ -0,0 +1,17 @@
+/**
+ * This is the coding convention. 
+ * This should be the description of the class (in case the classname was not clear enough)
+ */
+public class App {
+    /**
+     * This function does something.
+     * @param args Takes some arguments
+     * @return nothing
+     * @see a test thing
+     * @throws an Exception
+     */
+    public static void main(String[] args) throws Exception {
+        // Long comment\\I need space to describe something important.
+        System.out.println("Hello, World!"); // Short comment\\Short note
+    }
+}
