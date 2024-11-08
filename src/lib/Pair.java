0bce6c5a8ad81b818a6f4c929a4918401db64634
Developer <johndoe@johndoe.johndoe>
2024-11-08 18:32:39 -0500

    Pair.equals() impl

--- src/lib/Pair.java
+++ src/lib/Pair.java
@@ -5,4 +5,13 @@ package lib;
 public class Pair<T> {
     public T first;
     public T second;
+
+    /**
+     * Checks equality between two pairs
+     * @param in The pair to be compared to
+     * @return Whether the first and second elements of the first pair equal the first and second elements of the second pair, respectively.
+     */
+    public boolean equals(Pair<T> in) {
+        return in.first.equals(first) && in.second.equals(second);
+    }
 }
